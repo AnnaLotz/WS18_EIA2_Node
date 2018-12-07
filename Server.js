dd418f3323d0c9f049f07d8f784bab2439733cca
AnnaLotz <anna.lotz@web.de>
2018-12-07 16:27:06 +0100

    feierabend fr. 7.12.

--- Server.js
+++ Server.js
@@ -19,17 +19,16 @@ var A06_SendData;
             console.log("I hear voices!");
             _response.setHeader("content-type", "text/html; charset=utf-8");
             _response.setHeader("Access-Control-Allow-Origin", "*");
-            //            _response.write("requestUrl: " + _request.url);
             let query = Url.parse(_request.url, true).query;
             console.log("requestUrl: " + _request.url);
             console.log(query);
             for (let key in query) {
                 let product = query[key];
-                console.log(key + ": " + product.toString());
+                console.log(key + "= " + product.toString());
                 _response.write(key + " = " + product.toString() + "<br>");
             }
             _response.end();
         }
-    }
-})(A06_SendData || (A06_SendData = {}));
+    } //close handleRequest
+})(A06_SendData || (A06_SendData = {})); //close Namespace
 //# sourceMappingURL=Server.js.map
--- Server.ts
+++ Server.ts
@@ -30,8 +30,6 @@ namespace A06_SendData {
             _response.setHeader("content-type", "text/html; charset=utf-8");
             _response.setHeader("Access-Control-Allow-Origin", "*");
 
-//            _response.write("requestUrl: " + _request.url);
-
             let query: AssocProd = Url.parse(_request.url, true).query;
 
             console.log("requestUrl: " + _request.url);
@@ -39,12 +37,12 @@ namespace A06_SendData {
 
             for (let key in query) {
                 let product: string = query[key];
-                console.log(key + ": " + product.toString());
+                console.log(key + "= " + product.toString());
                 _response.write(key + " = " + product.toString() + "<br>");
             }
 
 
             _response.end();
         }
-    }
-}
+    } //close handleRequest
+} //close Namespace
